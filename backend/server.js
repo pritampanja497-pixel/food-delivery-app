a9fb01ae55511c8bc4a574c798b962f14fb49431
pritampanja497-pixel <pritampanja497@gmail.com>
2026-08-16 18:12:20 +0530

    update made

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,72 @@
-import 'dotenv/config'
+// import 'dotenv/config'
+// import express from "express";
+// import cors from "cors";
+// import connectDB from "./config/db.js"
+// import foodRouter from "./routes/foodRoute.js";
+// import userRouter from "./routes/userRoute.js";
+// import cartRouter from "./routes/cartRoute.js";
+// import orderRouter from "./routes/orderRoute.js";
+
+
+
+
+// const app = express()
+// const port = 4000
+
+
+// app.use(express.json())
+// app.use(cors())
+
+// connectDB()
+
+// app.use("/api/food", foodRouter)
+// app.use("/images", express.static('uploads'))
+// app.use("/api/user", userRouter)
+// app.use("/api/cart", cartRouter)
+// app.use("/api/order", orderRouter)
+
+
+// app.get("/",(req,res)=>{
+//     res.send("API Working")
+// })
+
+// app.listen(port,()=>{
+//   console.log(`Server Started on http://localhost:${port}`);
+  
+// })
+
+
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
+
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
+const app = express();
 
+app.use(express.json());
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
-const app = express()
-const port = 4000
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Food Delivery API Working" });
+});
 
-app.use(express.json())
-app.use(cors())
+connectDB();
 
-connectDB()
-
-app.use("/api/food", foodRouter)
-app.use("/images", express.static('uploads'))
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
-
-
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-
-app.listen(port,()=>{
-  console.log(`Server Started on http://localhost:${port}`);
-  
-})
-
+export default app;
