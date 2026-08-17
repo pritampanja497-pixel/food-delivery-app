1594db01906f99d2741d7248837a39602a7ce5ee
pritampanja497-pixel <pritampanja497@gmail.com>
2026-08-17 23:45:39 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,14 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
+import foodRouter from "./routes/foodRoute.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/food", foodRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
