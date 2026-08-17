bef1514469283db8f02c9766cf19cbdca55e8b31
pritampanja497-pixel <pritampanja497@gmail.com>
2026-08-17 23:00:11 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,42 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import express from "express";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+
+// import foodRouter from "./routes/foodRoute.js";
+// import userRouter from "./routes/userRoute.js";
+// import cartRouter from "./routes/cartRoute.js";
+// import orderRouter from "./routes/orderRoute.js";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/food", foodRouter);
+// app.use("/images", express.static("uploads"));
+// app.use("/api/user", userRouter);
+// app.use("/api/cart", cartRouter);
+// app.use("/api/order", orderRouter);
+
+// app.get("/", (req, res) => {
+//   res.send("API Working");
+// });
+
+// //connectDB();
+
+// export default app;
+
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js";
-
-import foodRouter from "./routes/foodRoute.js";
-import userRouter from "./routes/userRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads"));
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
-
 app.get("/", (req, res) => {
   res.send("API Working");
 });
-
-connectDB();
 
 export default app;
